61567cb4534e488fff2308870ac4a675d5116e9f
Jeff Denton <jeffdphoto@gmail.com>
2019-04-02 22:25:09 -0400

    Adds tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-44581526-9`,
       },
     },
     `gatsby-plugin-feed`,
